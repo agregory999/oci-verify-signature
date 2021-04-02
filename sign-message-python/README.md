7078e2a150cc08c9ab2aa9031baef781ab8d15bc
Andrew <andrew.gregory@oracle.com>
2021-04-02 15:34:11 -0400

    sign function

---
+++ sign-message-python/README.md
@@ -0,0 +1,29 @@
+# Message Signing with OCI Vault Encryption Key
+
+This function exists for the purpose of verifying a digitally signed messsge.  The message can be any text, JSON, or XML in nature.  The message is base64 encoded by the client, and this function calls the OCI KMS Crytpo service to produce a `signature`.  When the companion verify-signature function is invoked, it is able to re-calculate the same message digest (hash) that this function computes, and then verify that the provided signature (which this function returns) is valid. 
+
+## Client Flow
+The caller can take any file and create a base64-encoded string:
+```bash
+prompt> export INPUT=$(cat input.xml|base64)
+```
+Invoke the function like this, and it will return a `signature`:
+```bash
+prompt> echo '{"base64message":"${INPUT}"}'|fn -v invoke FunctionsApp sign-message-python
+{
+  "key_id": "ocid1.key.oc1.iad.bfqfhkltaaeuk.abuwcljsvls5p2jxws7rgg6rmv6wwhv4d2f3jwpy62moxqkj4zbxtdjdyeva",
+  "key_version_id": "ocid1.keyversion.oc1.iad.bfqfhkltaaeuk.aumtmbmqgsiaa.abuwcljsrsaiwd7zsw4oouaiejiwk3jmahexmyizjkmkdxozxuwtqsauh5yq",
+  "signature": "P8erJ54ducEPXRfS7h5noKUduZcK5IfX4rXiG7cxGfEmKn2A+XRq46YQnEbJd7EhJV4l/hmzknpJ6Tn3TblLwVMItxJhdd/M3yELcu1Ga42M3J3zGHDdgJ43iKUtD1XI7a6FZ5+e8A+iH88Ri1nRku0rT32bGI+5hPCNlnUdVFcy4vmwL8mRRStQKYFbfnH2o1E06fMFK1wP3x7mahXbHQ14B2sA6meRkeAXqPAQq4J6+79W2N9B6xUC7Bh76LTcwBERZMwzShCBLCVDVz6JCB1u4POtXkyRPv8BKzs6TqPy6P3Fs4E+KZVDYqd8ZmonKL6Qzb0oAAGKhVdgfUi8kQ==",
+  "signing_algorithm": "SHA_224_RSA_PKCS_PSS"
+}
+```
+In order to verify the signature, see the corresponding verification function:
+- [verify-signature] (../verify-signature-python)
+
+### Configuring Generic Function
+In order for this function to work, the following must be configured as function configs:
+- key_ocid
+- key_version_ocid
+- signing_algorithm
+- endpoint
+
---
+++ sign-message-python/func.py
@@ -0,0 +1,85 @@
+import io
+import json
+import logging
+import oci
+import hashlib
+import base64
+
+
+from fdk import response
+from oci.key_management.models import verified_data
+from oci.key_management.models.sign_data_details import SignDataDetails
+
+
+def handler(ctx, data: io.BytesIO = None):
+ 
+    # Define required config
+    try:
+        cfg = dict(ctx.Config())
+        key_ocid = cfg["key_ocid"]
+        logging.getLogger().info("Key OCID = " + key_ocid)
+        key_version_ocid = cfg["key_version_ocid"]
+        logging.getLogger().info("Key Version OCID = " + key_version_ocid)
+        signing_algorithm = cfg["signing_algorithm"]
+        logging.getLogger().info("Signing Algorithm = " + signing_algorithm)
+        endpoint = cfg["endpoint"]
+        logging.getLogger().info("Endpoint = " + endpoint)
+    except Exception as ex:
+        print('ERROR: Missing configuration keys required: key_ocid, key_version_ocid, signing_algorithm, endpoint', ex, flush=True)
+        return response.Response(
+            ctx, response_data=json.dumps(
+                {"error": 'Missing configuration keys required: key_ocid, key_version_ocid, signing_algorithm, endpoint'}),
+            headers={"Content-Type": "application/json"}
+        )
+
+    # Get Request   
+    try:
+        jsonrequest = json.loads(data.getvalue())
+        base64message = jsonrequest.get("base64message")
+    except Exception as ex:
+        print('ERROR: params JSON example: { "base64message":"xxx=="}', ex, flush=True)
+        return response.Response(
+            ctx, response_data=json.dumps(
+            {"error": 'Please send me JSON like this: { "base64message":"xxx=="}'}),
+            headers={"Content-Type": "application/json"}
+        )
+
+    # Access the KMS API
+    
+    try:
+        signer = oci.auth.signers.get_resource_principals_signer()
+        client = oci.key_management.KmsCryptoClient({}, signer=signer, service_endpoint=endpoint)
+
+        # Make the digest
+        digest = get_digest_from_string(base64message)
+
+	    # Sign the message
+        sign_response = client.sign(
+            sign_data_details=SignDataDetails(
+                key_id=key_ocid,
+                key_version_id=key_version_ocid,
+                signing_algorithm=signing_algorithm,
+                message=digest,
+                message_type="DIGEST"
+            )
+        )
+	        
+        logging.getLogger().info(sign_response.data)
+        # Return something
+        return response.Response(
+            ctx, response_data=sign_response.data,
+            headers={"Content-Type": "application/json"}
+        )
+
+    except (Exception, ValueError) as ex:
+        logging.getLogger().info('Error calling verify: ' + str(ex))
+        return response.Response(
+            ctx, response_data=ex,
+            headers={"Content-Type": "application/json"}
+        )
+#########Helper Methds########
+def get_digest_from_string(input):
+    m = hashlib.sha224()
+    m.update(str.encode(input))
+    digest = base64.b64encode(m.digest()).decode()
+    return digest
